4cdb6ac5e4230c0e4f5041ac8f2fab856d234612
n1k <nik726@gmail.com>
2020-06-10 21:16:40 +0700

    Добавляет определитель, что есть в ченджлоге, но нет в выгрузке.

--- main.py
+++ main.py
@@ -12,6 +12,24 @@ IC_GROUP2_INDICATORS = {
     'lb7': 'ЛБ 7.2'
 }
 
+CHANGELOG_TYPES = {
+    'ЛБv6': 'lb6',
+    'ЛБv6Pro': 'lbv6pro',
+    'ЛБv7': 'lb7'
+}
+
+# 'у О.В.': 'на сайте'
+REPLACERS = {
+    'CLASSIC': 'Релейный',
+    'iAStar': 'iASTAR',
+    'KONE_ESC': 'KONE ESC',
+    'SODIMAS_QI': 'SODIMAS QI',
+    'THYSSEN': 'THYSSEN TCM',
+    'FT9x0': 'THYSSEN FT9X',
+    'ШУЛК17': 'ШУЛК 17',
+    'ШУЛК32': 'ШУЛК 32'
+}
+
 
 def strip_text_in_list(lst: t.List, pattern='- \n') -> t.List[str]:
     new_lst = []
@@ -33,7 +51,7 @@ def is_block_end(line: str) -> bool:
 
 
 def extract_common_changelog(lst: t.List[str], k: int) -> common_log:
-    name = get_first_word(lst[k])
+    name = CHANGELOG_TYPES[get_first_word(lst[k])]
     _, version, _, date = lst[k + 1].split()
     version = dotted_str_to_int(version)
     log_list = []
@@ -47,6 +65,8 @@ def extract_common_changelog(lst: t.List[str], k: int) -> common_log:
 
 def extract_spec_changelog(lst: t.List[str], k: int) -> spec_log:
     name = lst[k].strip("- :")
+    if name in REPLACERS:
+        name = REPLACERS[name]
     log_list = []
     i = k + 1
     while not is_block_end(lst[i]):
@@ -111,25 +131,45 @@ def int_to_dotted_str(n: int) -> str:
     return '.'.join(str(n))
 
 
+def get_last_names(lst: t.List[dict], last_version: int, lb_type='lb7') -> set:
+    last_names = set()
+    for row in find_row(lst, last_version, lb_type):
+        last_names.add(row['IC_GROUP1'])
+    return last_names
+
+
 def main():
     source = file_to_list('multi_with_spec.txt')
 
-    common_logs = []
+    common_logs = {}
     for i in find_start_of_common_part(source):
-        common_logs.append(extract_common_changelog(source, i))
+        log = extract_common_changelog(source, i)
+        common_logs[log.name] = log
 
     for log in common_logs:
-        print(f"Пред. версию для {log.name}"
-              f" ищем по шаблону {IE_NAME_PATTERN.format(int_to_dotted_str(log.version - 1))}")
+        print(f"Пред. версию для {log}"
+              f" ищем по шаблону {IE_NAME_PATTERN.format(int_to_dotted_str(common_logs[log].version - 1))}")
 
+    spec_logs = {}
     for i in find_start_of_special_part(source):
-        print_spec(extract_spec_changelog(source, i))
+        log = extract_spec_changelog(source, i)
+        spec_logs[log.name] = log
 
     example = csv_to_list('from_lkds.ru.csv')
 
-    for row in find_row(example, 711, 'lb7'):
-        print(row['IE_XML_ID'])
-    print(len(list(find_row(example, 711, 'lb7'))))
+    spec_names = spec_logs.keys()
+
+    last_lb7_version = common_logs['lb7'].version - 1
+    last_names = get_last_names(example, last_lb7_version)
+
+    not_in = spec_names - last_names
+    if not_in:
+        print("ВАЖНО! Есть в списке изменений, но нет в выгрузке с сайта: ")
+        print(*not_in, sep='\n')
+
+    # for row in find_row(example, 711, 'lb7'):
+    #     print(row['IE_XML_ID'])
+
 
     # print(*source, sep='\n')
 
